ed755aa4e22c66d6f5332fac30a271e83658a662
☆ (ゝω・)v <35730970+matanki-saito@users.noreply.github.com>
2020-02-22 17:36:55 +0900

    ファイル名忘れ

--- env2file.py
+++ env2file.py
@@ -4,10 +4,10 @@ import os
 
 
 def main():
-    with open("", "w", encoding='utf-8') as f:
+    with open("credentials.json", "w", encoding='utf-8') as f:
         f.write(os.environ.get("CREDENTIALS_JSON"))
 
-    with open("", "w", encoding='utf-8') as f:
+    with open("settings.yaml", "w", encoding='utf-8') as f:
         f.write(os.environ.get("SETTINGS_YAML"))
 
 
